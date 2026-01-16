415e9d36e6b11c0ab8df08870e26b9641f521dea
Azuki.I <azuking69@hotmail.co.jp>
2026-01-16 22:46:49 +0900

    25.01.16 fixed class HttpException.php

--- app/Exceptions/HttpException.php
+++ app/Exceptions/HttpException.php
@@ -1,6 +1,6 @@
 <?php
     // 예외(Exception) 클라스 만들기
-    classHttpExceptionextendsException {
+    class HttpException extends Exception {
         // 생성자
         publicfunction__construct(publicint$status,string$message) {
             // 기존 구조를 부모에 맡긴다
