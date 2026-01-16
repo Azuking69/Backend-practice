a5af58c6ae737fc86ccf5dc87b4d19e0418f5f4a
Azuki.I <azuking69@hotmail.co.jp>
2026-01-16 22:25:13 +0900

    25.01.16 made HttpException.php

---
+++ app/Exceptions/HttpException.php
@@ -0,0 +1,10 @@
+<?php
+    // 예외(Exception) 클라스 만들기
+    classHttpExceptionextendsException {
+        // 생성자
+        publicfunction__construct(publicint$status,string$message) {
+            // 기존 구조를 부모에 맡긴다
+            parent::__construct($message);
+        }
+    }
+>
