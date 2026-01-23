2a5808ee50773ebf71cf62c83f48a34dd5f3fc8e
Azuki.I <azuking69@hotmail.co.jp>
2026-01-23 20:11:35 +0900

    26.01.23 made Routes.php

---
+++ back/routes/Routes.php
@@ -0,0 +1,23 @@
+<?php
+    // controller에 return하는 메소드
+    return function (AuthController $controller) {
+        // 정보를 받기
+        $method = $_SERVER['REQUEST_METHOD'];
+        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        // 회원가입의 경우
+        // POST로 '/auth/signup'까지 오면
+        if ($method === 'POST' && $path === '/auth/signup') {
+            $controller -> signup();
+            exit;
+        }
+
+        // 로그인의 경우
+        // POST로 '/auth/login'까지 오면
+        if ($method === 'POST' && $path === '/auth/login') {
+            $controller -> login();
+            exit;
+        }
+        // 어느 if에 안 맞으면 오류
+        JsonResponse::error(404, 'Not Found');
+    };
