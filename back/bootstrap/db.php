2f52b5aed724d562b50b5539d18e5adae95be282
Azuki.I <azuking69@hotmail.co.jp>
2026-01-27 18:20:50 +0900

    26.01.27 change db.php

--- back/bootstrap/db.php
+++ back/bootstrap/db.php
@@ -1,16 +1,14 @@
 <?php
-    // DB설계
-    $dsn = 'mysql: host = localhost; dbname=example; charset = utf8mb4';
-    $user = 'db_user';
-    $pass = 'db_pass';
+    // Docker용 DB설계
+    $host = getenv('DB_HOST') ?: 'db';
+    $db   = getenv('DB_NAME') ?: 'example';
+    $user = getenv('DB_USER') ?: 'db_user';
+    $pass = getenv('DB_PASS') ?: 'db_pass';
+
+    $dsn = "mysql:host={$host};dbname={$db};charset=utf8mb4";
 
     // PDO설계
-    $pdo = new PDO(
-        $dsn,
-        $user,
-        $pass,
-        [
-            PDO:: ATTR_ERRMODE => PDO:: ERRMODE_EXCEPTION,
-            PDO:: ATTR_DEFAULT_FETCH_MODE => PDO:: FETCH_ASSOC,
-        ]
-    );
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
