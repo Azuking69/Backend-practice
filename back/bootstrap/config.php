efcc159e197ea94f2641c4706cf0c1948570e2f5
Azuki.I <azuking69@hotmail.co.jp>
2026-01-27 18:21:49 +0900

    26.01.27 change config.php

--- back/bootstrap/config.php
+++ back/bootstrap/config.php
@@ -3,7 +3,7 @@
     // 설정된 데이터를 돌리는 파일
     return[
         // JWT서명에 쓰는 비밀키
-        'jwt_secret' => 'CHANGE_ME_TO_RANDOM_LONG_SECRET',
+        'jwt_secret' => getenv('JWT_SECRET'),
         // Token의 사용 기한
-        'jwt_exp_seconds' => 60 * 60,
+        'jwt_exp_seconds' => (int)getenv('JWT_EXP_SECONDS'),
     ];
